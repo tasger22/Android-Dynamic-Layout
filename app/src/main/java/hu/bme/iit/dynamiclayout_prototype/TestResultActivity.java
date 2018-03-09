6b50e1d697b77a3707921c8b3f8c75cc70de0da2
Stealth <tasger22@gmail.com>
2018-03-09 18:01:39 +0100

    TestResultActivity and layout added, layout almost done, new style for the result screen

---
+++ app/src/main/java/hu/bme/iit/dynamiclayout_prototype/TestResultActivity.java
@@ -0,0 +1,13 @@
+package hu.bme.iit.dynamiclayout_prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_result);
+    }
+}
